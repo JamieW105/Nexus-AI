f69975fc24be970732e8223771256f07a7ceeca1
JamieW105 <jamie.wells@stpats.school.nz>
2025-07-30 16:02:14 +1200

    Update

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../App';
+import { loader } from '@monaco-editor/react';
+
+// Configure monaco-editor loader to fetch assets from CDN
+loader.config({
+  paths: {
+    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.49.0/min/vs'
+  }
+});
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
